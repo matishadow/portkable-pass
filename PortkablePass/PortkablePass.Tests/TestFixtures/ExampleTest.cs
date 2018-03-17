a7a070cf2b8e2f82eaacb888c05b15b0b7ed387b
matishadow <matishadow@gmail.com>
2018-03-17 19:00:19 +0100

    Add Moq and NUnit nugets with some example code

---
+++ PortkablePass/PortkablePass.Tests/TestFixtures/ExampleTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace PortkablePass.Tests.TestFixtures
+{
+    [TestFixture]
+    public class ExampleTest
+    {
+        private int[] someArray;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            someArray = new[] {1, 2, 3};
+        }
+
+        [Test]
+        public void TestLengthEqualToThree()
+        {
+            Assert.AreEqual(someArray.Length, 3);
+        }
+
+        [TestCase(1, 2, 3, ExpectedResult = 6)]
+        [TestCase(2, -2, ExpectedResult = 0)]
+        [TestCase(2, 2, 2, ExpectedResult = 6)]
+        public int TestAddingWorks(params int[] elements)
+        {
+            return elements.Sum();
+        }
+    }
+}
