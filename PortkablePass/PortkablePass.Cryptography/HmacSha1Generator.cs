4a46b3c2dc2d9e4c484b0944617728f6e0ed1c92
matishadow <matishadow@gmail.com>
2018-03-19 22:46:42 +0100

    Add HmacSha1Generator with tests

---
+++ PortkablePass/PortkablePass.Cryptography/HmacSha1Generator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using PortkablePass.Interfaces.Cryptography;
+using PortkablePass.Interfaces.Encoding;
+
+namespace PortkablePass.Cryptography
+{
+    public class HmacSha1Generator : IHmacSha1Generator
+    {
+        private readonly IUtf8Converter utf8Converter;
+
+        public HmacSha1Generator(IUtf8Converter utf8Converter)
+        {
+            this.utf8Converter = utf8Converter;
+        }
+
+        public byte[] GenerateHmacSha1Hash(byte[] input, byte[] key)
+        {
+            CheckArguments(input, key);
+            var hmacsha1 = new HMACSHA1(key);
+
+            return hmacsha1.ComputeHash(input);
+        }
+
+        public byte[] GenerateHmacSha1Hash(string input, byte[] key)
+        {
+            CheckArguments(input, key);
+
+            byte[] inputBytes = utf8Converter.ConvertToBytes(input);
+
+            var hmacsha1 = new HMACSHA1(key);
+
+            return hmacsha1.ComputeHash(inputBytes);
+        }
+
+        private void CheckArguments(object input, byte[] key)
+        {
+            if (input == null || key == null)
+                throw new ArgumentNullException();
+            if (!key.Any())
+                throw new ArgumentException();
+        }
+    }
+}
