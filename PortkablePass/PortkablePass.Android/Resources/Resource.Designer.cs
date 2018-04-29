302643dbf20cc27988e49fabdb1bb58582eeca34
matishadow <matishadow@gmail.com>
2018-04-29 20:40:01 +0200

    Add icons

--- PortkablePass/PortkablePass.Android/Resources/Resource.Designer.cs
+++ PortkablePass/PortkablePass.Android/Resources/Resource.Designer.cs
@@ -158,11 +158,8 @@ namespace PortkablePass.Android
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
-			
 			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			public const int app_name = 2130968576;
 			
 			static String()
 			{
